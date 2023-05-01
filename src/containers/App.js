cca040158803e58551a6beff13ac2b6d624fe14f
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-05-01 23:53:34 +0200

    Enabled useState and useEffect to components

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,21 +6,16 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
 function App() {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: "",
-  //   };
-  // }
-
   const [robots, setRobots] = useState([]);
   const [searchField, setSearchField] = useState("");
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((users) => this.setState({ robots: users }));
-  // }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value);
